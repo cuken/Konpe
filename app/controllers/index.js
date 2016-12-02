022f1aa9f0fd4d057f292c3a716cb5581ba76b5e
Colin Uken <Colin Uken>
2016-12-02 01:24:19 -0700

    Initial Commit after Livestream 12-2-2016

---
+++ app/controllers/index.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+    emailAddress: '',
+    
+    isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+    isDisabled: Ember.computed.not('isValid'),
+    
+    actions: {
+        saveEmail() {
+            //Logic for saving the email to db.
+            this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+            this.set('emailAddress', '');
+        }
+    }
+
+});
